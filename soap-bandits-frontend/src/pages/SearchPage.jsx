ae6e7093025fd6082e19a3e829d5859026dfa665
Chassidy Wright <cnw5a@mtmail.mtsu.edu>
2026-04-19 13:47:38 -0400

    chore: cleaned up files

--- soap-bandits-frontend/src/pages/SearchPage.jsx
+++ soap-bandits-frontend/src/pages/SearchPage.jsx
@@ -1,4 +1,3 @@
-// This page may be removed from project (it's in our use cases.. so maybe not..)
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SearchPage.css';
